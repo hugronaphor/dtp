d7d7d972f20b32a2d360be2c73338f45e9726b80
Cornel Andreev <hugronaphor@gmail.com>
2014-06-02 21:48:59 +0300

    Fix scroll menu.

--- sites/all/modules/custom/dtp_common/js/scripts.js
+++ sites/all/modules/custom/dtp_common/js/scripts.js
@@ -40,11 +40,12 @@
 
   Drupal.behaviors.scrollMenu = {
     attach: function(context, settings) {
-
+      
       var topBarHeight = $('ul.menu').height();
 
-      $('a[href*=#]:not([href=#])').click(function(event) {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      $('ul.menu a[href*=#]:not([href=#])').click(function(event) {        
+        
+        //if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
           if (target.length) {
@@ -53,7 +54,7 @@
             }, 800);
             event.preventDefault();
           }
-        }
+        //}
       });
     }
   };
